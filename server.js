9c227acdef6723812a4ae905410544795d7369b0
Chalmiller <christopher@transcriptic.com>
2020-01-29 19:34:08 -0800

    Updating database connection

--- server.js
+++ server.js
@@ -20,10 +20,8 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-        host: 'postgresql-curly-59682',
-        user: 'Chris',
-        password: '',
-        database: 'smart-brain'
+        host: process.env.DATABASE_URL,
+        ssl: true,
     }
 });
 
